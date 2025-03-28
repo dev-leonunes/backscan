b8f510dac4f730e6f4fbeaab2cd147b3fd3b1df7
futuroDevLeo <leonunes07@outlook.com>
2025-03-28 16:29:37 -0300

    feat(env): add environment variables to secure sensitive information
    - Added `.env.example` as a template for environment variables.
    - Updated `.gitignore` to exclude `.env` from version control.
    - Modified `package.json` to support loading environment variables.
    - Updated `server.js` to utilize environment variables.

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const TELEGRAM_BOT_TOKEN = "BOT-TOKEN"; // Substitua pelo token do seu bot
-const TELEGRAM_CHAT_ID = "CHAT-TOKEN"; // Substitua pelo ID do chat (ou grupo) para onde quer enviar
+const TELEGRAM_BOT_TOKEN = process.env.BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.CHAT_ID;
 
 app.post("/send-location", async (req, res) => {
   const { latitude, longitude, maps } = req.body;
